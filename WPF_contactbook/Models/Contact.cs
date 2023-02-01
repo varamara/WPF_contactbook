e7f11c9dc097671a1c393a9c39902a9a07881bee
varamara <97692937+varamara@users.noreply.github.com>
2023-02-01 12:50:56 +0100

    Add project files.

---
+++ WPF_contactbook/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WPF_contactbook.Models;
+
+internal interface IContact
+{
+    Guid Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }
+    int PhoneNumber { get; set; }
+    string Address { get; set; }
+}
+internal class Contact : IContact
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int PhoneNumber { get; set; }
+    public string Address { get; set; } = null!;
+}
